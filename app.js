0cef30fb095af2863a558835880d0c91d53dc49a
Soulrcos <marcos.pkinho@gmail.com>
2024-12-19 10:48:58 -0300

    Finalização das tarefas

--- app.js
+++ app.js
@@ -3,29 +3,62 @@ let lista = [];
 const inputQuantidade = seleciona('#quantidade');
 const inputDe = seleciona('#de');
 const inputAte = seleciona('#ate');
-
-inputQuantidade.focus(); 
+const numerosSorteados = seleciona('.numeros_sorteados');
+const btn_reset = seleciona("#btn-reiniciar"); 
 
 function numeroAleatorio(q, d, a){
-    let aleatorio = (parseInt(Math.random() * a + 1));
+    let sufixo;
+    for (let i = 0; i < parseInt(q); i++){
+        sufixo = (i == q-1) ? ' ' : ' / ';
+        let aleatorio = (parseInt(Math.random() * a));
 
-    if (lista.length == a){
-        return lista = [];
-    } else if (lista.includes(aleatorio)){
-        return numeroAleatorio(q, d, a);
-    } else{
-        lista.push(aleatorio);
-        return console.log(aleatorio); 
+        if (lista.length > a){
+            lista = [];
+            i--;
+        }
+        else if (!lista.includes(aleatorio) && aleatorio >= d){
+            numerosSorteados.innerHTML += `${aleatorio}${sufixo}`;
+            lista.push(aleatorio);
+        } else{
+            i--;
+        }
+
+    }
+    lista = [];
+}
+
+function limpaTela(){
+    inputAte.value = '';
+    inputDe.value = '';
+    inputQuantidade.value = '';
+    lista = [];
+    numerosSorteados.innerHTML = "nenhum até agora";
+}
+
+function reiniciar(){
+    if (btn_reset.classList.contains('container__botao')){
+        btn_reset.setAttribute('class', 'container__botao-desabilitado');
+        limpaTela();
     }
 }
 
 function sortear() {
     let quantidadeNumeros = inputQuantidade.value;
     let deRange = inputDe.value;
-    let ateRange = inputAte.value; 
+    let ateRange = inputAte.value;
+    
+    if ([quantidadeNumeros, deRange, ateRange].every((e) => e != '')){
+        numerosSorteados.innerHTML = '';
+        numeroAleatorio(quantidadeNumeros, deRange, ateRange);
+        btn_reset.setAttribute('class', 'container__botao');
+    } else{
+        window.alert("[ERRO] OS CAMPOS NÃO PODEM ESTAR VAZIOS");
+    }
 
-    numeroAleatorio(quantidadeNumeros, deRange, ateRange);
+    
 }
+
+
 
 document.addEventListener('keydown', (e) => {
     if (e.key == 'Enter'){
